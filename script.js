6236a06897f99c8d16b7715f876c08ef4ddc4ed4
Leah Kim <86897050+Leahk1m@users.noreply.github.com>
2024-01-08 22:58:48 -0800

    added post request to form submit function

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-function handleRotate(){
-  var min = 1024;
-  var max = 9999;
-  var deg = Math.floor(Math.random() * (max - min)) + min;
-  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+function handleRotate(couponCode) {
 
-  var element = document.getElementById('mainbox');
-  element.classList.remove('animate');
+  const index = discountsArray.indexOf(couponCode);
 
-  setTimeout(function(){
-    element.classList.add('animate');
-  }, 5000);
+  console.log('discountsArray', discountsArray)
+  console.log('index', index)
+  const totalDiscounts = discountsArray.length;
+
+
+  const degreesPerDiscount = 360 / totalDiscounts;
+  const targetDegrees = index * degreesPerDiscount;
+
+
+  var minSpins = 3; 
+  var maxSpins = 6; 
+  var spins = Math.floor(Math.random() * (maxSpins - minSpins)) + minSpins;
+  var totalDegrees = spins * 360 + targetDegrees;
+
+
+  var box = document.getElementById('box');
+  box.style.transition = 'transform 5s ease-out';
+  box.style.transform = `rotate(${totalDegrees}deg)`;
 }
+
 
 document.getElementById('spin-to-win-form').addEventListener('submit', function(event) {
   event.preventDefault();
-  
-  handleRotate();
+
+  const formData = new FormData(this);
+  const payload = {
+    firstName: formData.get('firstName'),
+    lastName: formData.get('lastName'),
+    email: formData.get('email')
+  };
+
+  fetch('https://callbacks.dev.sakari.io/spintowin/13/contacts', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    const couponCode = data.data.offer.label; 
+
+    console.log('coupon code', couponCode)
+
+    handleRotate(couponCode);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 });
+
 
 
